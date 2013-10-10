c2f68dc7d1949e05b12043c72845ec1314f3bedb
gary g <tintin2000@yahoo.com>
2013-10-10 11:10:34 -0400

    Initial Commit

---
+++ Pirate Adventure/GGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GGAppDelegate.h
+//  Pirate Adventure
+//
+//  Created by me on 10/10/13.
+//  Copyright (c) 2013 me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Adventure/GGViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGViewController.h
+//  Pirate Adventure
+//
+//  Created by me on 10/10/13.
+//  Copyright (c) 2013 me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGViewController : UIViewController
+
+@end
